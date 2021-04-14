c31a5a68839ee753f977eec533685b66623e4e04
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-04-14 10:49:37 +0530

    Saved Files

---
+++ purgecss.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx"],
+  css: ["./src/assets/main.css"],
+};
--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className=" w-56 border border-gray-800 transition duration-300 p-2 my-2 dark:bg-gray-800 dark:text-white">
-        Toggle Light / Dark Mode
-      </div>
       <div className="bg-green-500 p-10 transition duration-300 text-3xl dark:bg-gray-800 dark:text-green-500">
         Welcome to test App
       </div>
