9a9255fb7283f65081c32f8c48037931ce674c95
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-04-15 20:01:18 +0530

    Food Details Page and Refactor

--- src/components/views/FoodDetails/FoodDetails.js
+++ src/components/views/FoodDetails/FoodDetails.js
@@ -1,7 +1,20 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 function FoodDetails() {
   let { id } = useParams();
-  return <div className="dark:text-white">My Food Details {id}</div>;
+  return (
+    <div className="dark:text-white">
+      <Link to="/">
+        <div
+          className="fixed right-0 flex items-center text-green-800 p-1 bg-green-50 dark:bg-transparent dark:text-green-100 hover:text-green-700 text-xs"
+          style={{ width: "fit-content" }}
+        >
+          <i className="pi pi-arrow-left mr-2" style={{ fontSize: "14px" }}></i>
+          <div>Back</div>
+        </div>
+      </Link>
+      <div className="h-screen">My Food Details {id}</div>
+    </div>
+  );
 }
 
 export default FoodDetails;
---
+++ src/components/views/Home/Home.js
@@ -0,0 +1,130 @@
+import ImageBox from "../../ImageBox/ImageBox";
+
+const images = [
+  {
+    fileName: "bhindi-curry",
+    name:
+      "how to make bhindi curry | bhindi subji | okra | ladies finger subji",
+    description: "",
+    link: "1",
+  },
+  {
+    fileName: "chole-batura",
+    name:
+      "how to make chole bhature recipe | chhole bhature | chana bhatura | chola batura",
+    description: "",
+    link: "2",
+  },
+  {
+    fileName: "chole-subji",
+    name:
+      "how to make pindi chole recipe | pindi chana masala | how to make chole pindi masala",
+    description: "",
+    link: "3",
+  },
+  {
+    fileName: "dahivada",
+    name:
+      "how to make dahi vada recipe | dahi bhalla recipe | soft dahi balle | dahi bade",
+    description: "",
+    link: "4",
+  },
+  {
+    fileName: "idly",
+    name:
+      "how to make idli with cooked rice recipe | idli with left over rice recipe",
+    description: "",
+    link: "5",
+  },
+  {
+    fileName: "kadai-veg",
+    name:
+      "how to make kadai paneer recipe | karahi paneer | how to make kadai paneer gravy",
+    description: "",
+    link: "6",
+  },
+  {
+    fileName: "masala-pav",
+    name:
+      "how to make masala pav recipe | bhaji masala pav | mumbai street style pav masala",
+    description: "",
+    link: "7",
+  },
+  {
+    fileName: "medu-vada",
+    name:
+      "how to make medu vada recipe in mixie | uddina vada | medhu vadai | ulundu vadai",
+    description: "",
+    link: "8",
+  },
+  {
+    fileName: "pepper-rasam",
+    name:
+      "how to make pepper rasam recipe | milagu rasam recipe | menasina saaru",
+    description: "",
+    link: "9",
+  },
+  {
+    fileName: "plain-dosa",
+    name: "how to make dosa recipe | plain dosa recipe | sada dosa recipe",
+    description: "",
+    link: "10",
+  },
+  {
+    fileName: "ribbon-pakora",
+    name:
+      "how to make ribbon pakoda recipe | ribbon murukku recipe | ola pakoda",
+    description: "",
+    link: "11",
+  },
+  {
+    fileName: "samosa",
+    name: "how to make samosa recipe | how to make samosa | aloo samosa",
+    description: "",
+    link: "12",
+  },
+  {
+    fileName: "semiya-upma",
+    name:
+      "how to make semiya upma recipe | vermicelli upma recipe | vermicelli recipe",
+    description: "",
+    link: "13",
+  },
+  {
+    fileName: "sweet-pongal",
+    name:
+      "how to make sweet pongal recipe | sakkarai pongal recipe | chakkara pongal",
+    description: "",
+    link: "14",
+  },
+  {
+    fileName: "tomato-rasam",
+    name:
+      "how to make instant rasam recipe | tomato rasam without dal | no dal rasam",
+    description: "",
+    link: "15",
+  },
+  {
+    fileName: "ven-pongal",
+    name:
+      "how to make ven pongal recipe | khara pongal recipe | how to make ven pongal",
+    description: "",
+    link: "16",
+  },
+];
+function Home() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 lg:justify-items-center">
+      {images.map((image) => (
+        <ImageBox
+          key={image.link}
+          fileName={image.fileName}
+          name={image.name}
+          link={image.link}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Home;
